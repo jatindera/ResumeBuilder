e1f54b20fae4aa871225931688663bd501d8734f
JA <jatinder.arora@gmail.com>
2025-01-14 12:48:16 +0530

    initial commit

--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.endpoints import resume_endpoints  # Make sure this import path is correct
-
-app = FastAPI()
-
-app.include_router(resume_endpoints.router, prefix="/api/v1", tags=["resume"]) 
